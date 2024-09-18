9e793da6bfb91653bc68b1e7da36b5529e372198
sarthak <sarthakgaonkar02@Sarthaks-Laptop.local>
2024-09-19 01:46:59 +0530

    New Changes

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the project directory if needed
+# cd /path/to/your/project
+
+# Add all changes
+git add .
+
+# Commit with a message
+git commit -m "New Changes"
+
+# Push to the master branch
+git push origin master
